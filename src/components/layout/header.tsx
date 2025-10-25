3def5f27d15ddf82ddfbc864394e607edab89b83
Joseph <josephosan1381@gmail.com>
2025-10-25 11:55:41 +0000

    Change the logo of the header and use something better the .ico on the

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -15,18 +15,16 @@ export function Header() {
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 256 256"
-              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="h-6 w-6 fill-primary"
             >
-              <rect width="256" height="256" fill="none" />
-              <path
-                d="M48.2,128.2,128,48.5l79.8,79.7a8,8,0,0,1-11.3,11.3L128,71.3,59.5,139.5a8,8,0,0,1-11.3-11.3Z"
-                className="fill-primary"
-              />
-              <path
-                d="M196.5,116.5,128,185,59.5,116.5a8,8,0,0,0-11.3,11.3L128,207.8l79.8-79.7a8,8,0,1,0-11.3-11.3Z"
-                className="fill-primary opacity-70"
-              />
+              <path d="M10 20.5 2 12l8-8.5" />
+              <path d="M14 3.5 22 12l-8 8.5" />
             </svg>
             <span className="font-bold font-headline sm:inline-block">
               Algo Blog
